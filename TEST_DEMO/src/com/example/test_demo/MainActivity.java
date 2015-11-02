cc81af3660066bc0b7735dfdc0cae3146b176726
yangxing711 <yangxing820@163.com>
2015-11-02 16:27:30 +0800

    再次提价

--- TEST_DEMO/src/com/example/test_demo/MainActivity.java
+++ TEST_DEMO/src/com/example/test_demo/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//哈喽，我又提交了
 	}
 
 	@Override
